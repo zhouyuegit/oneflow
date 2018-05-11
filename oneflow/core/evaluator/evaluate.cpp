0981a220679141a3aed9bb7e4091a387ee8fd134
bubuface0802 <bydu0802@gmail.com>
2018-05-11 15:01:24 +0800

    add the main entry file of evaluator

---
+++ oneflow/core/evaluator/evaluate.cpp
@@ -0,0 +1,69 @@
+#include "oneflow/core/job/runtime.h"
+#include "oneflow/core/common/str_util.h"
+#include "oneflow/core/common/protobuf.h"
+#include "oneflow/core/persistence/file_system.h"
+#include "oneflow/core/job/plan.pb.h"
+#include "oneflow/core/graph/task_node.h"
+
+namespace oneflow {
+
+class Evaluator final {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(Evaluator);
+  ~Evaluator() = default;
+
+  Evaluator(const Plan& plan, const int64_t actor_id);
+
+ private:
+  void NewAllGlobal();
+  void DeleteAllGlobal();
+};
+
+Evaluator::Evaluator(const Plan& plan, const int64_t actor_id) {
+  NewAllGlobal();
+  std::vector<const TaskProto*> eval_tasks;
+  int64_t this_machine_task_num = 0;
+
+  for (const TaskProto& task : plan.task()) {
+    if (task.machine_id() != 0) { continue; }
+    if (task.task_id() == actor_id) { eval_tasks.push_back(&task); }
+    this_machine_task_num += 1;
+  }
+  RuntimeCtx* runtime_ctx = Global<RuntimeCtx>::Get();
+  runtime_ctx->NewCounter("constructing_actor_cnt", this_machine_task_num);
+  HandoutTasks(eval_tasks);
+  runtime_ctx->WaitUntilCntEqualZero("constructing_actor_cnt");
+  LOG(INFO) << "All actor on this machine are constructed";
+
+  DeleteAllGlobal();
+}
+
+void Evaluator::NewAllGlobal() {
+  int64_t piece_num = 0;
+  Global<RuntimeCtx>::New(piece_num, false);
+}
+
+void Evaluator::DeleteAllGlobal() { Global<RuntimeCtx>::Delete(); }
+
+}  // namespace oneflow
+
+DEFINE_string(plan_filepath, "", "");
+DEFINE_string(actor_id, "", "");
+
+int main(int argc, char** argv) {
+  using namespace oneflow;
+  google::InitGoogleLogging(argv[0]);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  LocalFS()->RecursivelyCreateDirIfNotExist(LogDir());
+  RedirectStdoutAndStderrToGlogDir();
+  LOG(INFO) << "Evaluation Starting Up";
+
+  Plan plan;
+  LOG(INFO) << "Parse Plan File";
+  ParseProtoFromTextFile(FLAGS_plan_filepath, &plan);
+  Evaluator eval(plan, std::stoi(FLAGS_actor_id));
+
+  LOG(INFO) << "Evaluation Shutting Down";
+  CloseStdoutAndStderr();
+  return 0;
+}
