060e31963b1cc2cae55326d532f84f354a82f2b9
jamesplur <125025036@qq.com>
2018-10-15 17:07:28 +0800

    fix op

--- oneflow/core/operator/mask_tatget_op.cpp
+++ oneflow/core/operator/mask_tatget_op.cpp
@@ -6,26 +6,23 @@ void MaskTargetOp::InitFromOpConf() {
   CHECK_EQ(this->device_type(), DeviceType::kCPU);
   CHECK(op_conf().has_mask_target_conf());
   // Enroll input
-  EnrollInputBn("sample_rois"，false);
-  EnrollInputBn("sample_labels",false);
-  EnrollInputBn("seg_polys",false);
-  EnrollInputBn("seg_cls",false);
+  EnrollInputBn("sample_rois", false);
+  EnrollInputBn("sample_labels", false);
+  EnrollInputBn("seg_polys", false);
+  EnrollInputBn("seg_cls", false);
   // Enroll output
   EnrollOutputBn("mask_rois", false);
   EnrollOutputBn("masks", false);
   // Enroll data tmp
-  //EnrollDataTmpBn("boxes_index");
-  //EnrollDataTmpBn("max_overlaps");
-  //EnrollDataTmpBn("max_overlaps_gt_boxes_index");
+  // EnrollDataTmpBn("boxes_index");
+  // EnrollDataTmpBn("max_overlaps");
+  // EnrollDataTmpBn("max_overlaps_gt_boxes_index");
 }
 
-const PbMessage& MaskTargetOp::GetCustomizedConf() const {
-  return op_conf().mask_target_conf();
-}
+const PbMessage& MaskTargetOp::GetCustomizedConf() const { return op_conf().mask_target_conf(); }
 
-void MaskTargetOp::InferBlobDescs(
-    std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
-    const ParallelContext* parallel_ctx) const {
+void MaskTargetOp::InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+                                  const ParallelContext* parallel_ctx) const {
   const MaskTargetOpConf& conf = op_conf().mask_target_conf();
   // TODO: Check conf
   // input: sample_rois (n, r, 4) T
@@ -40,22 +37,27 @@ void MaskTargetOp::InferBlobDescs(
   int64_t image_num = sample_rois_blob_desc->shape().At(0);
   CHECK_EQ(image_num, seg_polys_blob_desc->shape().At(0));
   CHECK_EQ(image_num, seg_cls_blob_desc->shape().At(0));
+  CHECK_EQ(seg_polys_blob_desc->shape().At(1), seg_cls_blob_desc->shape().At(1));
   int64_t roi_num = sample_rois_blob_desc->shape().At(1);
+  CHECK_EQ(roi_num * image_num, sample_labels_blob_desc->shape().Count(0));
   int64_t fg_num = conf.num_rois_per_image();
   int64_t class_num = conf.num_classes();
   int64_t max_gt_boxes_num = conf.max_gt_boxes_num();
   int64_t M = conf.resolution();
 
-  DataType data_type = rpn_rois_blob_desc->data_type();
+  DataType data_type = sample_rois_blob_desc->data_type();
   // output: mask_rois (n * fg_num, 5) T
   BlobDesc* mask_rois_blob_desc = GetBlobDesc4BnInOp("mask_rois");
   mask_rois_blob_desc->mut_shape() = Shape({image_num * fg_num, 5});
   mask_rois_blob_desc->set_data_type(data_type);
-  mask_rois_blob_desc->
+  mask_rois_blob_desc->set_has_dim0_valid_num_field(true);
+  mask_rois_blob_desc->mut_dim0_inner_shape() = Shape({1, image_num * fg_num});
   // output: masks (n * fg_num, class_num , M , M) T
   BlobDesc* masks_blob_desc = GetBlobDesc4BnInOp("masks");
-  masks_blob_desc->mut_shape() = Shape({image_num * fg_num, class_num, M , M});
+  masks_blob_desc->mut_shape() = Shape({image_num * fg_num, class_num, M, M});
   masks_blob_desc->set_data_type(data_type);
+  masks_blob_desc->set_has_dim0_valid_num_field(true);
+  masks_blob_desc->mut_dim0_inner_shape() = Shape({1, image_num * fg_num});
 }
 
 REGISTER_OP(OperatorConf::kMaskTargetConf, MaskTargetOp);
