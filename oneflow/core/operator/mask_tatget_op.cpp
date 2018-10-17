b04c3ff376d820ba2413943661e70b2319451108
jamesplur <plur@bupt.edu.cn>
2018-10-17 17:43:04 +0800

    change op

--- oneflow/core/operator/mask_tatget_op.cpp
+++ oneflow/core/operator/mask_tatget_op.cpp
@@ -25,51 +25,48 @@ void MaskTargetOp::InferBlobDescs(std::function<BlobDesc*(const std::string&)> G
                                   const ParallelContext* parallel_ctx) const {
   const MaskTargetOpConf& conf = op_conf().mask_target_conf();
   // TODO: Check conf
-  // input: sample_rois (n, r, 4) T
+  // input: sample_rois (R, 5) T
   const BlobDesc* sample_rois_blob_desc = GetBlobDesc4BnInOp("sample_rois");
-  // input: sample_labels (n * r) int32_t
+  // input: sample_labels (R) int32_t
   const BlobDesc* sample_labels_blob_desc = GetBlobDesc4BnInOp("sample_labels");
   // input: seg_polys (n,g,b) float
   const BlobDesc* seg_polys_blob_desc = GetBlobDesc4BnInOp("seg_polys");
   // input: seg_cls (n,g) int32_t
   const BlobDesc* seg_cls_blob_desc = GetBlobDesc4BnInOp("seg_cls");
 
-  int64_t image_num = sample_rois_blob_desc->shape().At(0);
-  CHECK_EQ(image_num, seg_polys_blob_desc->shape().At(0));
-  CHECK_EQ(image_num, seg_cls_blob_desc->shape().At(0));
   CHECK_EQ(seg_polys_blob_desc->shape().At(1), seg_cls_blob_desc->shape().At(1));
-  int64_t roi_num = sample_rois_blob_desc->shape().At(1);
-  CHECK_EQ(roi_num * image_num, sample_labels_blob_desc->shape().Count(0));
-  int64_t fg_num = conf.num_rois_per_image();
+  int64_t R = sample_rois_blob_desc->shape().At(0);
+  int64_t N = seg_polys_blob_desc->shape().At(0);
+  int64_t roi_per_img = conf.num_rois_per_image();
   int64_t class_num = conf.num_classes();
   int64_t max_gt_boxes_num = conf.max_gt_boxes_num();
   int64_t M = conf.resolution();
 
   DataType data_type = sample_rois_blob_desc->data_type();
-  // output: mask_rois (n * fg_num, 5) T
+  // output: mask_rois (fgR, 5) T
   BlobDesc* mask_rois_blob_desc = GetBlobDesc4BnInOp("mask_rois");
-  mask_rois_blob_desc->mut_shape() = Shape({image_num * fg_num, 5});
+  mask_rois_blob_desc->mut_shape() = Shape({N * roi_per_img, 5});
   mask_rois_blob_desc->set_data_type(data_type);
   mask_rois_blob_desc->set_has_dim0_valid_num_field(true);
-  mask_rois_blob_desc->mut_dim0_inner_shape() = Shape({1, image_num * fg_num});
+  mask_rois_blob_desc->mut_dim0_inner_shape() = Shape({1, N * roi_per_img});
   // output: masks (n * fg_num, class_num , M , M) T
   BlobDesc* masks_blob_desc = GetBlobDesc4BnInOp("masks");
-  masks_blob_desc->mut_shape() = Shape({image_num * fg_num, class_num, M, M});
+  masks_blob_desc->mut_shape() = Shape({N * roi_per_img, class_num, M, M});
   masks_blob_desc->set_data_type(data_type);
   masks_blob_desc->set_has_dim0_valid_num_field(true);
-  masks_blob_desc->mut_dim0_inner_shape() = Shape({1, image_num * fg_num});
+  masks_blob_desc->mut_dim0_inner_shape() = Shape({1, N * roi_per_img});
   // data tmp: roi_index (rois_num) int32_t
   BlobDesc* boxes_index_blob_desc = GetBlobDesc4BnInOp("rois_index");
-  boxes_index_blob_desc->mut_shape() = Shape({rois_num});
+  boxes_index_blob_desc->mut_shape() = Shape({R});
   boxes_index_blob_desc->set_data_type(DataType::kInt32);
   // data tmp: max_overlaps (rois_num) float
   BlobDesc* max_overlaps_blob_desc = GetBlobDesc4BnInOp("max_overlaps");
-  max_overlaps_blob_desc->mut_shape() = Shape({rois_num});
+  max_overlaps_blob_desc->mut_shape() = Shape({R});
   max_overlaps_blob_desc->set_data_type(DataType::kFloat);
   // data tmp: max_overlaps_mask_boxes_index (rois_num) int32_t
   BlobDesc* max_overlaps_mask_boxes_index_blob_desc =
       GetBlobDesc4BnInOp("max_overlaps_mask_boxes_index");
-  max_overlaps_mask_boxes_index_blob_desc->mut_shape() = Shape({rois_num});
+  max_overlaps_mask_boxes_index_blob_desc->mut_shape() = Shape({R});
   max_overlaps_mask_boxes_index_blob_desc->set_data_type(DataType::kInt32);
 }
 
