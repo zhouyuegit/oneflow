a0ffbbf9dd33b99c29c5a400c2afece5f60e6b07
Jin Yi <xxmyj.k@gmail.com>
2019-01-24 14:47:38 +0800

    feat: add json fmt with header

--- oneflow/core/common/plog_util.h
+++ oneflow/core/common/plog_util.h
@@ -1,6 +1,8 @@
 #ifndef ONEFLOW_CORE_COMMON_PLOG_UTIL_H
 #define ONEFLOW_CORE_COMMON_PLOG_UTIL_H
 
+#define PLOG_CAPTURE_FILE
+
 #include <plog/Log.h>
 #include <plog/Util.h>
 #include <plog/Record.h>
@@ -10,13 +12,32 @@ namespace oneflow {
   class JsonFmt {
     public:
       static util::nstring header() {
-        return util::nstring();
+        util::nostringstream ss;
+
+        ss << "["
+           << "\"" << "message" << "\", "
+           << "\"" << "ts"      << "\", "
+           << "\"" << "tid"     << "\", "
+           << "\"" << "level"   << "\", "
+           << "\"" << "file"    << "\", "
+           << "\"" << "line"    << "\", "
+           << "\"" << "func"    << "\""
+           << "]"  << "\n";
+        return ss.str();
       }
 
       static util::nstring format(const Record& record) {
         util::nostringstream ss;
 
-        ss << record.getMessage() << "\n";
+        ss << "{"
+           << "\"message\": " << "\"" << record.getMessage() << "\", "
+           << "\"ts\": "      << "\"" << record.getTime().time*1000 + record.getTime().millitm << "\", "
+           << "\"tid\": "     << "\"" << record.getTid() << "\", "
+           << "\"level\": "   << "\"" << severityToString(record.getSeverity()) << "\", "
+           << "\"file\": "    << "\"" << record.getFile() << "\", "
+           << "\"line\": "    << "\"" << record.getLine() << "\", "
+           << "\"func\": "    << "\"" << record.getFunc() << "@" << record.getObject() << "\""
+           << "}" << "\n";
         return ss.str();
       }
   };
