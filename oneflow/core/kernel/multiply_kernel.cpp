95896f80f45e3a133b84bacaf0764b5a9d5a0b03
guo-ran <360112263@qq.com>
2019-07-20 11:21:01 +0800

    fix multiply backward

--- oneflow/core/kernel/multiply_kernel.cpp
+++ oneflow/core/kernel/multiply_kernel.cpp
@@ -22,15 +22,19 @@ void MultiplyKernel<device_type, T>::BackwardDataContent(
   const Blob* in_0_blob = BnInOp2Blob("in_0");
   Blob* in_0_diff_blob = BnInOp2Blob("in_0_diff");
   const Blob* in_1_blob = BnInOp2Blob("in_1");
-  // Blob* in_1_diff_blob = BnInOp2Blob("in_1_diff");
-  // in_1_diff = out_diff * in_0
-  // KernelUtil<device_type, T>::Mul(ctx.device_ctx, in_0_blob->shape().elem_cnt(),
-  //                                 in_0_blob->dptr<T>(), out_diff_blob->dptr<T>(),
-  //                                 in_1_diff_blob->mut_dptr<T>());
-  // in_0_diff = out_diff * in_1
-  KernelUtil<device_type, T>::Mul(ctx.device_ctx, in_1_blob->shape().elem_cnt(),
-                                  in_1_blob->dptr<T>(), out_diff_blob->dptr<T>(),
-                                  in_0_diff_blob->mut_dptr<T>());
+  Blob* in_1_diff_blob = BnInOp2Blob("in_1_diff");
+  if (in_1_diff_blob) {
+    // in_1_diff = out_diff * in_0
+    KernelUtil<device_type, T>::Mul(ctx.device_ctx, in_0_blob->shape().elem_cnt(),
+                                    in_0_blob->dptr<T>(), out_diff_blob->dptr<T>(),
+                                    in_1_diff_blob->mut_dptr<T>());
+  }
+  if (in_0_diff_blob) {
+    // in_0_diff = out_diff * in_1
+    KernelUtil<device_type, T>::Mul(ctx.device_ctx, in_1_blob->shape().elem_cnt(),
+                                    in_1_blob->dptr<T>(), out_diff_blob->dptr<T>(),
+                                    in_0_diff_blob->mut_dptr<T>());
+  }
 }
 
 template<DeviceType device_type, typename T>
