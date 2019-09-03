ac6f61466e472d79f663811b118e254b3977d471
Shiyuan Shang-Guan <shgshy@gmail.com>
2019-09-03 13:07:31 +0800

    update function

--- oneflow/python/framework/function.py
+++ oneflow/python/framework/function.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 import functools
 import oneflow.python.framework.job_set_util as job_set_util
 from oneflow.python.framework.session import Session
+from oneflow.python.oneflow_export import oneflow_export
 
 _job_name2job_func = {}
 
@@ -12,9 +13,9 @@ def function(job_func):
     def Func(*args, **kwargs):
         Session().run(job_func(*args, **kwargs))
     for x in dir(job_func):
-        if x.startwith('__oneflow_'):
-            setattr(Func, x, getattr(func, x))
+        if x.startswith('__oneflow_'):
+            setattr(Func, x, getattr(job_func, x))
     if job_func.__name__ not in _job_name2job_func:
-        job_set_util.add_job(func(*args, **kwargs))
-        _job_name2job_func[func.__name__] = job_func
+        job_set_util.add_job(job_func)
+        _job_name2job_func[job_func.__name__] = job_func
     return Func
