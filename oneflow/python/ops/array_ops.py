b19c544a0ccfdeb5d5393c0a7ed96be572fe440b
liujuncheng <liujuncheng1022@gmail.com>
2019-10-29 13:33:42 +0800

    mutable_broadcast_parallel

--- oneflow/python/ops/array_ops.py
+++ oneflow/python/ops/array_ops.py
@@ -272,7 +272,7 @@ def parallel_cast(
         if type(dist) is distribute_util.SplitDistribute:
             setattr(conf.split_parallel, "axis", dist.axis)
         elif type(dist) is distribute_util.BroadcastDistribute:
-            getattr(conf, "broadcast_parallel")
+            conf.mutable_broadcast_parallel()
         else:
             raise NotImplementedError
     if distribute is not None:
