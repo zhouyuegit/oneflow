229e43740b6d02ae1b149a6aac2581e6db7ca1d1
liujuncheng <liujuncheng1022@gmail.com>
2019-10-31 20:30:32 +0800

    IDX

--- oneflow/core/kernel/batch_gather_kernel_util.cu
+++ oneflow/core/kernel/batch_gather_kernel_util.cu
@@ -17,12 +17,12 @@ __device__ int64_t GetInOffset(const IDX out_offset, const K* indices, const IDX
   return batch_idx * gather_dim_size * instance_size + idx * instance_size + inner_idx;
 }
 
-template<typename T, typename K>
-__global__ void BatchGatherForwardGpu(const int64_t elem_cnt, const T* in, const K* indices,
-                                      const int64_t indices_num, const int64_t instance_size,
-                                      const int64_t gather_dim_size, T* out) {
+template<typename T, typename K, typename IDX>
+__global__ void BatchGatherForwardGpu(const IDX elem_cnt, const T* in, const K* indices,
+                                      const IDX indices_num, const IDX instance_size,
+                                      const IDX gather_dim_size, T* out) {
   CUDA_1D_KERNEL_LOOP(i, elem_cnt) {
-    out[i] = in[GetInOffset<K, int32_t>(i, indices, indices_num, instance_size, gather_dim_size)];
+    out[i] = in[GetInOffset<K, IDX>(i, indices, indices_num, instance_size, gather_dim_size)];
   }
 }
 
@@ -43,11 +43,16 @@ void BatchGatherKernelUtilImpl<DeviceType::kGPU, T, K>::Forward(DeviceCtx* ctx, 
   const int64_t batch_num = flat_out_shape.At(0);
   const int64_t indices_num = flat_out_shape.At(1);
   const int64_t instance_size = flat_out_shape.At(2);
-
   const int64_t elem_cnt = batch_num * indices_num * instance_size;
-  BatchGatherForwardGpu<T, K>
-      <<<BlocksNum4ThreadsNum(elem_cnt), kCudaThreadsNumPerBlock, 0, ctx->cuda_stream()>>>(
-          elem_cnt, in, indices, indices_num, instance_size, gather_dim_size, out);
+  if (elem_cnt > GetMaxVal<int32_t>() / 2) {
+    BatchGatherForwardGpu<T, K, int64_t>
+        <<<BlocksNum4ThreadsNum(elem_cnt), kCudaThreadsNumPerBlock, 0, ctx->cuda_stream()>>>(
+            elem_cnt, in, indices, indices_num, instance_size, gather_dim_size, out);
+  } else {
+    BatchGatherForwardGpu<T, K, int32_t>
+        <<<BlocksNum4ThreadsNum(elem_cnt), kCudaThreadsNumPerBlock, 0, ctx->cuda_stream()>>>(
+            elem_cnt, in, indices, indices_num, instance_size, gather_dim_size, out);
+  }
 }
 
 #define INSTANTIATE_BATCH_GATHER_KERNEL_UTIL_IMPL_GPU(in_type_pair, index_type_pair)          \
