dbac176f891e0517502e26f7ad02b4b91df390bc
liujuncheng <liujuncheng1022@gmail.com>
2019-11-01 10:58:07 +0800

    backward

--- oneflow/core/kernel/gather_kernel_util.cu
+++ oneflow/core/kernel/gather_kernel_util.cu
@@ -37,18 +37,24 @@ __global__ void GatherForwardGpu(IDX elem_cnt, const K* indices, IDX num_indices
   }
 }
 
-template<typename T, typename K>
-__global__ void GatherBackwardGpu(int64_t elem_cnt, const K* indices, int64_t num_indices,
-                                  const T* out_diff, int64_t gather_dim_size,
-                                  int64_t inner_dim_size, T* in_diff, const int64_t offset) {
-  CUDA_1D_KERNEL_LOOP(i, elem_cnt) {
+template<typename T, typename K, typename IDX>
+__global__ void GatherBackwardGpu(IDX elem_cnt, const K* indices, IDX num_indices,
+                                  const T* out_diff, IDX gather_dim_size, IDX inner_dim_size,
+                                  T* in_diff, const IDX offset) {
+  CUDA_1D_KERNEL_LOOP_T(IDX, i, elem_cnt) {
     const T diff_val = out_diff[i];
     if (diff_val != static_cast<T>(0)) {
       const int64_t in_offset =
-          GetInOffset<K, int64_t>(i, indices, num_indices, gather_dim_size, inner_dim_size, offset);
+          GetInOffset<K, IDX>(i, indices, num_indices, gather_dim_size, inner_dim_size, offset);
       if (in_offset >= 0) { gpu_atomic_add(in_diff + in_offset, diff_val); }
     }
   }
+}
+
+bool IsSafeUseIndex32(const Shape& flat_in_shape, const int64_t num_indices) {
+  const int64_t in_elem_cnt = flat_in_shape.elem_cnt();
+  const int64_t out_elem_cnt = flat_in_shape.At(0) * num_indices * flat_in_shape.At(2);
+  return std::max(out_elem_cnt, in_elem_cnt) < GetMaxVal<int32_t>() / 2;
 }
 
 }  // namespace
@@ -57,15 +63,14 @@ template<typename T, typename K>
 struct GatherKernelUtilImpl<DeviceType::kGPU, T, K> final {
   static void Forward(DeviceCtx* ctx, const K* indices, int64_t num_indices, const T* in,
                       const Shape& flat_in_shape, T* out, const int64_t offset) {
-    const int64_t in_elem_cnt = flat_in_shape.elem_cnt();
     const int64_t out_elem_cnt = flat_in_shape.At(0) * num_indices * flat_in_shape.At(2);
-    if (std::max(out_elem_cnt, in_elem_cnt) > GetMaxVal<int32_t>() / 2) {
-      GatherForwardGpu<T, K, int64_t>
+    if (IsSafeUseIndex32(flat_in_shape, num_indices)) {
+      GatherForwardGpu<T, K, int32_t>
           <<<BlocksNum4ThreadsNum(out_elem_cnt), kCudaThreadsNumPerBlock, 0, ctx->cuda_stream()>>>(
               out_elem_cnt, indices, num_indices, in, flat_in_shape.At(1), flat_in_shape.At(2), out,
               offset);
     } else {
-      GatherForwardGpu<T, K, int32_t>
+      GatherForwardGpu<T, K, int64_t>
           <<<BlocksNum4ThreadsNum(out_elem_cnt), kCudaThreadsNumPerBlock, 0, ctx->cuda_stream()>>>(
               out_elem_cnt, indices, num_indices, in, flat_in_shape.At(1), flat_in_shape.At(2), out,
               offset);
@@ -74,10 +79,17 @@ struct GatherKernelUtilImpl<DeviceType::kGPU, T, K> final {
   static void Backward(DeviceCtx* ctx, const K* indices, int64_t num_indices, const T* out_diff,
                        const Shape& flat_in_shape, T* in_diff, const int64_t offset) {
     const int64_t elem_cnt = flat_in_shape.At(0) * num_indices * flat_in_shape.At(2);
-    GatherBackwardGpu<T, K>
-        <<<BlocksNum4ThreadsNum(elem_cnt), kCudaThreadsNumPerBlock, 0, ctx->cuda_stream()>>>(
-            elem_cnt, indices, num_indices, out_diff, flat_in_shape.At(1), flat_in_shape.At(2),
-            in_diff, offset);
+    if (IsSafeUseIndex32(flat_in_shape, num_indices)) {
+      GatherBackwardGpu<T, K, int32_t>
+          <<<BlocksNum4ThreadsNum(elem_cnt), kCudaThreadsNumPerBlock, 0, ctx->cuda_stream()>>>(
+              elem_cnt, indices, num_indices, out_diff, flat_in_shape.At(1), flat_in_shape.At(2),
+              in_diff, offset);
+    } else {
+      GatherBackwardGpu<T, K, int64_t>
+          <<<BlocksNum4ThreadsNum(elem_cnt), kCudaThreadsNumPerBlock, 0, ctx->cuda_stream()>>>(
+              elem_cnt, indices, num_indices, out_diff, flat_in_shape.At(1), flat_in_shape.At(2),
+              in_diff, offset);
+    }
   }
 };
 
@@ -87,7 +99,7 @@ struct GatherKernelUtilImpl<DeviceType::kGPU, float16, K> final {
   static void Forward(DeviceCtx* ctx, const K* indices, int64_t num_indices, const float16* in,
                       const Shape& flat_in_shape, float16* out, const int64_t offset) {
     const int64_t elem_cnt = flat_in_shape.At(0) * num_indices * flat_in_shape.At(2);
-    GatherForwardGpu<half, K>
+    GatherForwardGpu<half, K, int64_t>
         <<<BlocksNum4ThreadsNum(elem_cnt), kCudaThreadsNumPerBlock, 0, ctx->cuda_stream()>>>(
             elem_cnt, indices, num_indices, reinterpret_cast<const half*>(in), flat_in_shape.At(1),
             flat_in_shape.At(2), reinterpret_cast<half*>(out), offset);
@@ -96,7 +108,7 @@ struct GatherKernelUtilImpl<DeviceType::kGPU, float16, K> final {
                        const float16* out_diff, const Shape& flat_in_shape, float16* in_diff,
                        const int64_t offset) {
     const int64_t elem_cnt = flat_in_shape.At(0) * num_indices * flat_in_shape.At(2);
-    GatherBackwardGpu<half, K>
+    GatherBackwardGpu<half, K, int64_t>
         <<<BlocksNum4ThreadsNum(elem_cnt), kCudaThreadsNumPerBlock, 0, ctx->cuda_stream()>>>(
             elem_cnt, indices, num_indices, reinterpret_cast<const half*>(out_diff),
             flat_in_shape.At(1), flat_in_shape.At(2), reinterpret_cast<half*>(in_diff), offset);
