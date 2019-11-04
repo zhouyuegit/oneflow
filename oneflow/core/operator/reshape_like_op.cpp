c33dc68ba3c4019fe62656f730e4a7e985f59581
liujuncheng <liujuncheng1022@gmail.com>
2019-11-04 19:39:30 +0800

    ReshapeLikeOp

--- oneflow/core/operator/reshape_like_op.cpp
+++ oneflow/core/operator/reshape_like_op.cpp
@@ -1,8 +1,31 @@
-#include "oneflow/core/operator/reshape_like_op.h"
+#include "oneflow/core/operator/operator.h"
 #include "oneflow/core/job/sbp_signature_builder.h"
 #include "oneflow/core/operator/reshape_op_util.h"
 
 namespace oneflow {
+
+class ReshapeLikeOp final : public Operator {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(ReshapeLikeOp);
+  ReshapeLikeOp() = default;
+  ~ReshapeLikeOp() override = default;
+
+  void InitFromOpConf() override;
+  const PbMessage& GetCustomizedConf() const override;
+
+  Maybe<void> InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+                             const ParallelContext* parallel_ctx) const override;
+
+ private:
+  Maybe<void> InferBatchAxis(
+      std::function<OptInt64*(const std::string&)> BatchAxis4BnInOp) const override {
+    return NaiveInferBatchAxis(BatchAxis4BnInOp);
+  }
+
+  Maybe<void> GetSbpSignatures(
+      const std::function<Maybe<const BlobDesc*>(const std::string&)>& LogicalBlobDesc4Ibn,
+      const ParallelDesc& parallel_desc, SbpSignatureList* sbp_sig_list) const override;
+};
 
 void ReshapeLikeOp::InitFromOpConf() {
   CHECK(op_conf().has_reshape_like_conf());
