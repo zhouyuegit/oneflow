611757a159305e21db41e491cb2ade6b051bf5b2
liujuncheng <liujuncheng1022@gmail.com>
2019-11-04 19:40:29 +0800

    BatchGatherOp

--- oneflow/core/operator/batch_gather_op.cpp
+++ oneflow/core/operator/batch_gather_op.cpp
@@ -1,7 +1,28 @@
-#include "oneflow/core/operator/batch_gather_op.h"
+#include "oneflow/core/operator/operator.h"
 #include "oneflow/core/job/sbp_signature_builder.h"
 
 namespace oneflow {
+
+class BatchGatherOp final : public Operator {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(BatchGatherOp);
+  BatchGatherOp() = default;
+  ~BatchGatherOp() override = default;
+
+  void InitFromOpConf() override;
+  const PbMessage& GetCustomizedConf() const override;
+  Maybe<void> InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+                             const ParallelContext* parallel_ctx) const override;
+
+ private:
+  Maybe<void> GetSbpSignatures(
+      const std::function<Maybe<const BlobDesc*>(const std::string&)>& LogicalBlobDesc4Ibn,
+      SbpSignatureList* sbp_sig_list) const override;
+  Maybe<void> InferBatchAxis(
+      std::function<OptInt64*(const std::string&)> BatchAxis4BnInOp) const override {
+    return NaiveInferBatchAxis(BatchAxis4BnInOp);
+  }
+};
 
 void BatchGatherOp::InitFromOpConf() {
   CHECK(op_conf().has_batch_gather_conf());
