0bdb56b7701772262a6bbb6dabd2b88a517d4e3e
liujuncheng <liujuncheng1022@gmail.com>
2019-11-04 19:37:19 +0800

    broadcast remove header

--- oneflow/core/operator/broadcast_mul_op.cpp
+++ oneflow/core/operator/broadcast_mul_op.cpp
@@ -1,6 +1,19 @@
-#include "oneflow/core/operator/broadcast_mul_op.h"
+#include "oneflow/core/operator/broadcast_binary_op.h"
 
 namespace oneflow {
+
+class BroadcastMulOp final : public BroadcastBinaryOp {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(BroadcastMulOp);
+  BroadcastMulOp() = default;
+  ~BroadcastMulOp() override = default;
+
+ private:
+  const PbMessage& GetCustomizedConf() const override;
+  Maybe<void> VirtualGetSbpSignatures(
+      const std::function<Maybe<const BlobDesc*>(const std::string&)>& LogicalBlobDesc4Ibn,
+      SbpSignatureList* sbp_sig_list) const override;
+};
 
 const PbMessage& BroadcastMulOp::GetCustomizedConf() const {
   return op_conf().broadcast_mul_conf();
