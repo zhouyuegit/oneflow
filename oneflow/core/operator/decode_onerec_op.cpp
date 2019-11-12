39b24d9f09c0b188b20a1d2721b16ec917fe83e8
liujuncheng <liujuncheng1022@gmail.com>
2019-11-12 16:34:18 +0800

    remove out

--- oneflow/core/operator/decode_onerec_op.cpp
+++ oneflow/core/operator/decode_onerec_op.cpp
@@ -14,9 +14,6 @@ class DecodeOneRecOp final : public Operator {
   void InitFromOpConf() override;
   const PbMessage& GetCustomizedConf() const override;
   LogicalNode* NewProperLogicalNode() const override { return new PrintLogicalNode; }
-  Maybe<void> InferOutBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
-                                const ParallelContext*, const SbpSignature* sbp_signature,
-                                std::function<void(OpContext*)> EnrollOpCtx) const override;
   Maybe<void> InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
                              const ParallelContext* parallel_ctx,
                              const SbpSignature* sbp_signature) const override;
@@ -50,25 +47,6 @@ Maybe<void> DecodeOneRecOp::InferBlobDescs(
     const int64_t num_output_axes = 1 + field_conf.output_shape().dim_size();
     std::vector<int64_t> dim_vec(num_output_axes);
     dim_vec[0] = device_batch_size;
-    FOR_RANGE(int64_t, j, 1, num_output_axes) { dim_vec[j] = field_conf.output_shape().dim(j - 1); }
-    out_blob_desc->mut_shape() = Shape(dim_vec);
-    out_blob_desc->set_data_type(field_conf.output_data_type());
-  }
-  return Maybe<void>::Ok();
-}
-
-Maybe<void> DecodeOneRecOp::InferOutBlobDescs(
-    std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp, const ParallelContext*,
-    const SbpSignature* sbp_signature, std::function<void(OpContext*)> EnrollOpCtx) const {
-  const DecodeOneRecOpConf& conf = op_conf().decode_onerec_conf();
-  const int64_t batch_size = conf.batch_size();
-  CHECK_EQ(output_bns().size(), conf.field_size());
-  FOR_RANGE(int64_t, i, 0, output_bns().size()) {
-    BlobDesc* out_blob_desc = GetBlobDesc4BnInOp(output_bns().Get(i));
-    const DecodeOneRecFieldConf& field_conf = conf.field().Get(i);
-    const int64_t num_output_axes = 1 + field_conf.output_shape().dim_size();
-    std::vector<int64_t> dim_vec(num_output_axes);
-    dim_vec[0] = batch_size;
     FOR_RANGE(int64_t, j, 1, num_output_axes) { dim_vec[j] = field_conf.output_shape().dim(j - 1); }
     out_blob_desc->mut_shape() = Shape(dim_vec);
     out_blob_desc->set_data_type(field_conf.output_data_type());
