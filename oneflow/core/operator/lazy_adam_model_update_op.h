37ad23c79d72c92e36091cf6daa2797d6912d716
liujuncheng <liujuncheng1022@gmail.com>
2019-11-30 15:22:43 +0800

    fix

--- oneflow/core/operator/lazy_adam_model_update_op.h
+++ oneflow/core/operator/lazy_adam_model_update_op.h
@@ -20,6 +20,9 @@ class LazyAdamModelUpdateOp final : public NormalModelUpdtOp {
   Maybe<void> MdUpdtVirtualInferBlobDescs(
       std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
       const ParallelContext* parallel_ctx) const override;
+  const HashSet<std::string> AlwaysBroadcastParallelBns() const override {
+    return HashSet<std::string>();
+  }
 };
 
 }  // namespace oneflow
