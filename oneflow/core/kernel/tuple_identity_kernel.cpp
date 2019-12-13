e522ca9a9a2eb91e3e8af1266fe3b3d4a211bd57
liujuncheng <liujuncheng1022@gmail.com>
2019-12-13 15:25:43 +0800

    ForwardDim0ValidNum

--- oneflow/core/kernel/tuple_identity_kernel.cpp
+++ oneflow/core/kernel/tuple_identity_kernel.cpp
@@ -3,6 +3,23 @@
 namespace oneflow {
 
 template<DeviceType device_type>
+void TupleIdentityKernel<device_type>::ForwardDim0ValidNum(
+    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  const auto& input_bns = this->op_attribute().input_bns();
+  const auto& output_bns = this->op_attribute().output_bns();
+  CHECK_EQ(input_bns.size(), output_bns.size());
+  FOR_RANGE(int, i, 0, input_bns.size()) {
+    const Blob* in_blob = BnInOp2Blob(input_bns.Get(i));
+    Blob* out_blob = BnInOp2Blob(output_bns.Get(i));
+    if (in_blob->has_dim0_valid_num_field()) {
+      CHECK(out_blob->has_dim0_valid_num_field());
+      CHECK_EQ(in_blob->dim0_inner_shape(), out_blob->dim0_inner_shape());
+      out_blob->CopyDim0ValidNumFrom(ctx.device_ctx, in_blob);
+    }
+  }
+}
+
+template<DeviceType device_type>
 void TupleIdentityKernel<device_type>::ForwardDataContent(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
   const auto& input_bns = this->op_attribute().input_bns();
@@ -10,7 +27,7 @@ void TupleIdentityKernel<device_type>::ForwardDataContent(
   CHECK_EQ(input_bns.size(), output_bns.size());
   FOR_RANGE(int, i, 0, input_bns.size()) {
     Blob* out_blob = BnInOp2Blob(output_bns.Get(i));
-    out_blob->CopyDataContentFrom(ctx.device_ctx, BnInOp2Blob(input_bns.Get(i)));
+    out_blob->CopyValidDataContentFrom(ctx.device_ctx, BnInOp2Blob(input_bns.Get(i)));
   }
 }
 
